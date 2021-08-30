87f2cc95e30cf9690755e5a79ddbc81a36ab9052
Prithiv SP <prithivspn@gmail.com>
2021-08-30 20:50:41 +0530

    Made some changes in parallax section at Our Mission Page

--- src/components/Our-Misson-Page/OMParallax.jsx
+++ src/components/Our-Misson-Page/OMParallax.jsx
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import './OMParallax.css';
+import React, { Component } from "react";
+import "./OMParallax.css";
 // importing bootstrap css
 import "bootstrap/dist/css/bootstrap.css";
 
 class OMParallax extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div class="parallax">
-                <h1>Help educate a child to help a family  
-                    <span>#change</span>
-                    <span id="span-a">a</span>
-                    <span>life</span>
-                </h1>
-            </div>
-         );
-    }
+  state = {};
+  render() {
+    return (
+      <div class="OMParallax">
+        <h1>
+          Help educate a child to <span className="OMParallax-help">help</span>{" "}
+          a family
+          <span>#change</span>
+          <span id="span-a">a</span>
+          <span>life</span>
+        </h1>
+      </div>
+    );
+  }
 }
- 
+
 export default OMParallax;
